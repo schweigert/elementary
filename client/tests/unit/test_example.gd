5a2dee0d565c7373983fa923de170107a883ca7c
Marlon Henry Schweigert <fleyhe0@gmail.com>
2018-09-02 23:25:28 -0300

    Tests

---
+++ client/tests/unit/test_example.gd
@@ -0,0 +1,12 @@
+extends 'res://addons/gut/test.gd'
+
+class TestFeatureA:
+	extends 'res://addons/gut/test.gd'
+
+
+	func setup():
+		pass
+
+
+	func test_something():
+		assert_true(true, 'shold be true')
